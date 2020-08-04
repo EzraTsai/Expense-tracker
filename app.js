e1170e773fba79b6cc0097fa52b5cf5c6b850197
Ezra Tsai <ishow8166@icloud.com>
2020-08-04 16:23:38 +0800

    feat: modify index.hbs and add handlebar helper

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Handlebars = require('handlebars')
+const Record = require('./models/record')
+const Category = require('./models/category')
+const bodyParser = require('body-parser')
+const calculateMoney = require('./calculate')
 const app = express()
 
 mongoose.connect('mongodb://localhost/exprense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,11 +18,27 @@ db.once('open', () => {
     console.log('mongodb conneted!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+Handlebars.registerHelper('checkIfSam', function (category, currentCategory, options) {
+    if (category === currentCategory) {
+        return options.fn(this)
+    } 
+    return options.inverse(this)
+  })
+
 app.get('/', (req, res) => {
-    res.render('index')
+    Record.find()
+        .lean()
+        .sort({ date: 'asc' })
+        .then(records => {
+            const totalAmount = records.map(record => record.amount).reduce((accumulator, currentValue) => { return accumulator + currentValue })
+            res.render('index', { records, totalAmount })
+        })
+        .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
