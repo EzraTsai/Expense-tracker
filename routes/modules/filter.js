4061e68e85b89436da0b3aaab9be48bded1665b3
Ezra Tsai <ishow8166@icloud.com>
2020-08-05 21:18:47 +0800

    fix: filter function

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -5,46 +5,37 @@ const Category = require('../../models/category')
 
 //filter
 router.get('/:id', (req, res) => {
-    // const id = req.params.id
-    // Category.find()
-    //     .lean()
-    //     .sort({ _id: 'asc' })
-    //     .then(categories => {
-    //         categories.forEach(category => {
-    //             categoryList.push({
-    //                 name: category.name
-    //             })
-    //         })
-    //     })
-    //     .catch(error => console.log(error))
-
-    // return Promise.all(promises).then(() => {
-    //     Record.find(condition)
-    //         .lean()
-    //         .then((records) => {
-    //             if (keyword !== '-1') categoryList[keyword].check = true
-    //             const totalAmount = records.map(record => record.amount).reduce((prev, curr) => prev + curr)
-    //             res.render('index', { records, totalAmount, categoryList })
-    //         })
-    // })
-    // const id = req.params.id
-    // return Category.findById(id)
-    //     .lean()
-    //     .then(categories => {
-    //         console.log(categories.categoryName)
-    //         Record.find()
-    //             .lean()
-    //             .sort({ date: 'desc' })
-    //             .then(records => {
-    //                 if (categories.categoryName === records.category) {
-    //                    const totalAmount = records.map(record => record.amount).reduce((accumulator, currentValue) => { return accumulator + currentValue })
-    //                 res.render('index', { categories, totalAmount, records }) 
-    //                 }
-    //             })
-    //             .catch(error => console.log(error))
-    //     })
-    //     .catch(error => console.log(error))
-    res.render('index')
+    const id = req.params.id
+    Category.findByIdAndUpdate(id)
+        .lean()
+        .sort({ _id: 'asc' })
+        .then(categories => {
+            Record.find()
+                .lean()
+                .sort({ date: 'desc' })
+                .then(recordlist => {
+                    let categoryAmount = [0]
+                    let categoryList = []
+                    for (a in recordlist ) {  
+                        if (categories.categoryName === recordlist [a].category) {
+                            categoryAmount.push(recordlist [a].amount)
+                            categoryList.push(recordlist [a])
+                        }
+                    }
+                    const records = categoryList
+                    const totalAmount = categoryAmount.reduce((accumulator, currentValue) => { return accumulator + currentValue })
+                    Category.find()
+                    .lean()
+                    .sort({ _id: 'asc' })
+                    .then( categorieslist => {
+                        const categories = categorieslist
+                        res.render('index', { totalAmount, records, categories })
+                    })
+                    .catch(error => console.log(error))
+                })
+                .catch(error => console.log(error))
+        })
+        .catch(error => console.log(error))
 })
 
 module.exports = router
