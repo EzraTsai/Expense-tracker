f147a082b59ed21947e71e36a704c6b2bc7df6a8
Ezra Tsai <ishow8166@icloud.com>
2020-08-25 23:00:24 +0800

    feat: add user login fature via passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
 const Handlebars = require('handlebars')
@@ -7,12 +8,19 @@ const Category = require('./models/category')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const userPassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
 
+app.use(session({
+    secret:'MySecret',
+    resave: false,
+    saveUninitialized: true
+}))
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+userPassport(app)
 app.use(routes)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
