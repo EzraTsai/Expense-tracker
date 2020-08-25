c245f7d13fb271e31973d2dc7c5a6ea4a6e7e102
Ezra Tsai <ishow8166@icloud.com>
2020-08-25 21:40:57 +0800

    feat: add sing up feature

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/records', records)
 router.use('/filter', filter)
+router.use('/users', users)
 
 module.exports = router
